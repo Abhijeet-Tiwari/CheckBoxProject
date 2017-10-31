54a91468f2e05b3599441ab7bc454ee1abc19042
Abhijeet Tiwari <abhijeet96tiwari@gmail.com>
2017-10-31 10:32:42 +0530

    SecondCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {NgForm} from '@angular/forms';
 @Component({
     selector: 'my-app',
     templateUrl: `app/app.component.html`,
@@ -6,7 +7,21 @@ import {Component} from '@angular/core';
   })
 
   export class AppComponent {
-  
+    onSubmit(f:NgForm) {
+      var options = f.value;
+      var map = {optionA: 1, optionB: 2, optionC: 3, optionD: 4};
+      var result = "selected options are : ";
+      for( var prop in options) {
+        if(options[prop] === true){
+          result = result + map[prop] + "," ;
+          // alert(map[prop] + " is selected");
+        }
+        // if(options[prop]){ }
+      }
+
+      alert(result);
+      console.log(f.value);  
+    }
   }
     
   
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
-
+import {FormsModule} from '@angular/forms';
 import { AppComponent }  from './app.component';
 
 
-
+  
 @NgModule({
-  imports:      [ BrowserModule, RouterModule ],
+  imports:      [ BrowserModule, RouterModule,FormsModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent]
 })
